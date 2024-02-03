4c88de7c9f292b722eaf061a956604d7f98fe1dd
DendyGrobovshik <idenis.gradoboev@yandex.com>
2024-02-03 16:06:31 +0300

    fix description of strange behavior

--- hw_rgb2yuv/float_bug_m32.c
+++ hw_rgb2yuv/float_bug_m32.c
@@ -9,10 +9,10 @@ typedef struct {
   uint8_t b;
 } RGB;
 
-// gcc -m32 bug.c -o bug && ./bug
-// > x=255.000000, y=255, z=254
+// gcc float_bug_m32.c -o bug && ./bug
+// > x=255.000000, y=255, z=255
 
-// gcc -m32 bug.c -o bug && ./bug
+// gcc -m32 float_bug_m32.c -o bug && ./bug
 // > x=255.000000, y=255, z=254
 void foo(RGB rgb) {
   float x = (0.2990 * rgb.r + 0.5870 * rgb.g + 0.1140 * rgb.b);
